7801a6f9f78968838055901ffc202d86df9d0894
ordidxzero <dev@dlwlrma.net>
2023-06-05 19:43:24 +0900

    남은 시간이 0 이하일 경우 게임이 종료되고 리셋되도록 수정하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import GameBoard from './GameBoard';
 import Header from './Header';
 
 const App = () => {
+  const intervalId = useRef<null | number>(null);
   const [stage, setStage] = useState(1);
   const [remainingTime, setRemainingTime] = useState(15);
   const [score, setScore] = useState(0);
+
+  const resetState = useCallback(() => {
+    setStage(1);
+    setRemainingTime(15);
+    setScore(0);
+  }, []);
+
+  useEffect(() => {
+    if (intervalId.current != null) {
+      clearInterval(intervalId.current);
+      setTimeout(() => {
+        intervalId.current = window.setInterval(() => {
+          setRemainingTime(prevRemainingTime => prevRemainingTime - 1);
+        }, 1000);
+      }, 0);
+    }
+  }, [stage]);
+
+  useEffect(() => {
+    intervalId.current = window.setInterval(() => {
+      setRemainingTime(prevRemainingTime => prevRemainingTime - 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId.current ?? undefined);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (remainingTime <= 0) {
+      alert(`GAME OVER! \n STAGE: ${stage} / SCORE: ${score}`);
+      resetState();
+    }
+  }, [remainingTime]);
 
   const GAME_SIZE = Math.round((stage + 0.5) / 2) + 1;
 
@@ -17,7 +51,7 @@ const App = () => {
   }, [stage, remainingTime]);
 
   const onWrongBlockClick = useCallback(() => {
-    setRemainingTime(prevRemainingTime => prevRemainingTime - 3);
+    setRemainingTime(prevRemainingTime => (prevRemainingTime - 3 < 0 ? 0 : prevRemainingTime - 3));
   }, []);
 
   return (
